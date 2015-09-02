48e5cc0ced707b7b8691d101ea858e29616f45b8
YaroslavO <yaroslavo@softwareplanetgroup.com>
2015-09-02 12:35:33 +0300

    testing

--- app/controllers/application_controller.js
+++ app/controllers/application_controller.js
@@ -1,3 +1,3 @@
 before('protect from forgery', function () {
-  protectFromForgery('25072abcae7286a0e58b43e0a7cbe83bd8b997aa');
+  //protectFromForgery('25072abcae7286a0e58b43e0a7cbe83bd8b997aa');
 });
